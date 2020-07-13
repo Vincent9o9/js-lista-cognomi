8f21ef172e981b22fb15883d49be3309a9381f08
Vincent <23vincent86@gmail.com>
2020-07-13 17:53:15 +0200

    end format

--- js/script.js
+++ js/script.js
@@ -9,14 +9,17 @@
 var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 
 var utente = prompt('inserisci il tuo cognome');
+var format = utente.charAt(0).toUpperCase() + utente.slice(1).toLowerCase();
 
-listaCognomi.push(utente);
+listaCognomi.push(format);
 listaCognomi.sort();
 
-var posizioneUmana = listaCognomi.indexOf(utente) + 1;
+var posizioneUmana = listaCognomi.indexOf(format) + 1;
+
 
 for (var i=0; i<listaCognomi.length; i++) {
     document.getElementById('list').innerHTML += '<li>' + listaCognomi[i] + '</li>';
 }
+
 
 document.getElementById('position').innerHTML = 'la tua posizione umana = ' + posizioneUmana;
