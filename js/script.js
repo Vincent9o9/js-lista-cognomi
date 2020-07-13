e50ac5dd1b480ff6a7e6d8e8fb8aff9ce3aac41d
Vincent <23vincent86@gmail.com>
2020-07-13 17:18:11 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert();
+// Chiedere all’utente il cognome
+
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+
+// stampa la lista ordinata alfabeticamente
+
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+
+var utente = prompt('inserisci il tuo cognome');
+
+listaCognomi.push(utente);
+listaCognomi.sort();
+
+var posizioneUmana = listaCognomi.indexOf(utente) + 1;
+
+for (var i=0; i<listaCognomi.length; i++) {
+    document.getElementById('list').innerHTML += '<li>' + listaCognomi[i] + '</li>';
+}
+
+document.getElementById('position').innerHTML = 'la tua posizione umana = ' + posizioneUmana;
